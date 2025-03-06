815e737d4446176fcc3380eca5ae9df06b9a88e9
Adan Ernesto Vela <adan.vela@ucf.edu>
2025-03-05 20:03:39 -0500

    Update buildImage to include latest tag

--- flask2mongo/buildImage.sh
+++ flask2mongo/buildImage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build -t adclab/flask2mongo:v0.4.0 .
-docker push adclab/flask2mongo:v0.4.0
+docker build -t adclab/flask2mongo:v0.4.3 -t adclab/flask2mongo:latest .
+docker push adclab/flask2mongo:v0.4.3
+docker push adclab/flask2mongo:latest
--- keyDB/buildimage.sh
+++ keyDB/buildimage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build -t adclab/keydb:v0.4.1 .
+docker build -t adclab/keydb:v0.4.1 -t adclab/keydb:latest .
 docker push adclab/keydb:v0.4.1
+docker push adclab/keydb:latest
